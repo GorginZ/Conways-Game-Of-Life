5aac3539cbfe7b5725ea005a1ffa196225d7dab9
GorginZ <georgia.z.leng@gmail.com>
2020-09-15 17:28:35 +1000

    run through console first time, make world, print world and innoculate world all working

--- ConwaysGameOfLife/Program.cs
+++ ConwaysGameOfLife/Program.cs
@@ -2,13 +2,33 @@
 
 namespace ConwaysGameOfLife
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-            var world = new World(10,20);
-            world.PrintWorld();
-        }
+      Console.WriteLine("Welcome to Life Game!");
+      Console.WriteLine("A new world has been made.");
+      Console.WriteLine("how many rows should be in this world?");
+      var inputRows = Console.ReadLine();
+
+      Console.WriteLine("how many columns should be in this world?");
+      var inputCols = Console.ReadLine();
+
+      var tryParseRows = int.TryParse(inputRows, out int rows);
+      var tryParseCols = int.TryParse(inputRows, out int cols);
+
+
+      var world = new World(rows, cols);
+      world.PrintWorld();
+
+      Console.WriteLine("innoculate world with some live cells");
+      Console.WriteLine("input your coordinates in the following format: 0,0 0,1 0,2 4,4 2,2 ");
+
+      var coords = Console.ReadLine();
+      world.Populate(coords);
+
+      Console.Clear();
+      world.PrintWorld();
     }
+  }
 }
