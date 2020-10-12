3866a724dab63d871c8ad40e622a5e7d7017f352
GorginZ <georgia.z.leng@gmail.com>
2020-10-13 10:06:49 +1100

    added images and made a start on Readme

--- ConwaysGameOfLife/Program.cs
+++ ConwaysGameOfLife/Program.cs
@@ -13,16 +13,23 @@ namespace ConwaysGameOfLife
     {
       Console.BackgroundColor = ConsoleColor.DarkGray;
       Console.ForegroundColor = ConsoleColor.Red;
+      Console.Clear();
       var userInput = new UserInput();
       int rowDimension;
       int columnDimension;
       List<Index> coordinateList;
 
-      Console.Write("Welcome to Conways Game of Life.\n \nJohn Horton Conway has contributed immensely to many fields of mathematical theory and his Game of Life is one of his most famous and fun contributions which presents fun and interesting challenges for programmers and captures interest beyond those already enaged in mathematical theory. \n\nJohn Conway passed away in April 2020 just three days after developing COVID19 symptoms. This is a huge loss to the mathematical community and to the world more broadly. RIP John Conway. \n\n In the Game of Life we set a world of cells and apply a series of rules to them in each 'tick', the tick represents the discrete moment in time where the state of the world changes. This tick changes the state, living or dead, of all of the cells.\n\nthe rules: \n\n1. Any live cell with fewer than two live neighbours dies, as if by underpopulation. \n2. Any live cell with two or three live neighbours lives on to the next generation. \n3. Any live cell with more than three live neighbours dies, as if by overpopulation. \n4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. \n\nYou will set dimensions of the world and the initial state of the cells by selecting coordinates and watch how they behave.");
+      var introString = "\nWelcome to Conways Game of Life.\n \nJohn Horton Conway has contributed immensely to many fields of mathematical theory and his Game of Life is one of his most famous and fun contributions which presents fun and interesting challenges for programmers and captures interest beyond those already enaged in mathematical theory. \n\nJohn Conway passed away in April 2020 just three days after developing COVID19 symptoms. This is a huge loss to the mathematical community and to the world more broadly. RIP John Conway. \n\n In the Game of Life we set a world of cells and apply a series of rules to them in each 'tick', the tick represents the discrete moment in time where the state of the world changes. This tick changes the state, living or dead, of all of the cells.\n\nthe rules: \n\n1. Any live cell with fewer than two live neighbours dies, as if by underpopulation. \n2. Any live cell with two or three live neighbours lives on to the next generation. \n3. Any live cell with more than three live neighbours dies, as if by overpopulation. \n4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. \n\nYou will set dimensions of the world and the initial state of the cells by selecting coordinates and watch how they behave.";
+      Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (introString.Length / 2)) + "}", introString));
 
-      Console.WriteLine("How many rows in the world?");
+
+      var rowPrompt = "\nHow many rows in the world?";
+      Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (rowPrompt.Length / 2)) + "}", rowPrompt));
+
       rowDimension = GetValidDimensions();
-      Console.WriteLine("How many columns in the world?");
+      var columnPrompt = "\nHow many columns in the world?";
+      Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + (columnPrompt.Length / 2)) + "}", columnPrompt));
+
       columnDimension = GetValidDimensions();
       var world = new World(rowDimension, columnDimension);
 
--- README.md
+++ README.md
@@ -1,1 +1,46 @@
 # ConwaysGameOfLife
+
+
+
+### Life
+
+This C# console app is my first shot at Conways Game of Life. I've been interested in this problem since studying discrete maths during my diploma of I.T. 
+
+I was very sad to learn recently that John Conway passed away in April 2020 just days after coming down with symptoms of COVID19.
+
+Life is how I know about John Conway's work, and is  certainly his most widely known contribution to maths theory - while this seems to have been be a point of frustration, to become synonymous with one work, it is wondrous to have contributed something to the world of maths and programming theory which is able to engage and excite people who are not already part of those worlds. 
+
+
+
+### The Rules
+
+1. Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+2. Any live cell with two or three live neighbours lives on to the next generation. 
+3.  Any live cell with more than three live neighbours dies, as if by overpopulation. 
+4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction. 
+
+
+
+### Console Play Through
+
+An introduction and rules.
+
+User sets the dimensions of the world and then 'innoculates' the world with live cells. The user inputs a string of coordinates: eg: "0,0 0,1 0,2", which sets the starting state of the world, then the program begins to 'tick' and print the new state of the world.
+
+
+
+<img src="docs/intro.png">
+
+
+
+### Tick
+
+The tick in Life is the discrete moment where the rules are actioned and the state of the world transforms.
+
+My console app runs an infinite loop wherein it ticks and displays the state of the world. It will do this until the program is terminated.
+
+<img src="docs/tick1.png">
+
+
+
+<img src="docs/tick2.png">
